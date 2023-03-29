22aa6a6117bf71d915cbbf837613c9b1dcd74159
Ksenia Melashenko <112662076+ksenmel@users.noreply.github.com>
2023-03-29 19:23:43 +0300

    Implement 'searchNode' and 'insertNode' methods (#7)
    Add an implementation of the ``searchNode`` and ``insertNode`` methods to search for a node and insert it into the binary search tree.

--- lib/src/main/kotlin/bstrees/AbstractBSTrees.kt
+++ lib/src/main/kotlin/bstrees/AbstractBSTrees.kt
@@ -7,12 +7,21 @@ abstract class BinarySearchTree<T : Comparable<T>, NodeType : TreeNode<T, NodeTy
     protected var treeRoot: NodeType? = null
 
     fun search(data: T): T? = searchNode(data)?.data
+
+    /**
+     * Searches for node and returns it as a result (or null).
+     */
     protected fun searchNode(data: T): NodeType? {
-        // searches for node and returns it as a result (or null)
-        // aside from public 'search' method
-        // it might be useful for deletion implementations to find the node to delete
-        // that's why we're keeping it visible to inherited classes
-        TODO("Not yet implemented")
+        var currentNode = treeRoot
+        while (currentNode != null) {
+            val res = data.compareTo(currentNode.data)
+            currentNode = when {
+                res < 0 -> currentNode.left
+                res > 0 -> currentNode.right
+                else -> return currentNode
+            }
+        }
+        return null
     }
 
     open fun insert(data: T) {
@@ -20,11 +29,43 @@ abstract class BinarySearchTree<T : Comparable<T>, NodeType : TreeNode<T, NodeTy
     }
 
     protected abstract fun createNewNode(data: T): NodeType
+
+    /**
+     * Does simple insert and returns inserted node
+     * Uses [createNewNode] to create new node to insert
+     */
     protected fun insertNode(data: T): NodeType {
-        // does simple insert and returns inserted node
-        // uses 'createNewNode' to create new node to insert
-        // will be used in different implementations of trees as part of the insert process
-        TODO("Not yet implemented")
+
+        if (treeRoot == null) {
+            val createdNode = createNewNode(data)
+            treeRoot = createdNode
+            return createdNode
+        }
+
+        var currentNode = treeRoot!!
+        while (true) {
+            val res = data.compareTo(currentNode.data)
+            if (res < 0) {
+                if (currentNode.left == null) {
+                    val createdNode = createNewNode(data)
+                    currentNode.left = createdNode
+                    createdNode.parent = currentNode
+                    return createdNode
+                }
+                currentNode = currentNode.left!!
+            } else if (res > 0) {
+                if (currentNode.right == null) {
+                    val createdNode = createNewNode(data)
+                    currentNode.right = createdNode
+                    createdNode.parent = currentNode
+                    return createdNode
+                }
+                currentNode = currentNode.right!!
+            } else {
+                currentNode.data = data
+                return currentNode
+            }
+        }
     }
 
     abstract fun delete(data: T): T?
