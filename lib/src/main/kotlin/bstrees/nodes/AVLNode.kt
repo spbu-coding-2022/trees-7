56cdfed6a21d75a229115a84cd9fa5f881875d07
Andrei <15161335+therain7@users.noreply.github.com>
2023-04-01 22:05:32 +0300

    fix(AVLNode): change height default value to 1

--- lib/src/main/kotlin/bstrees/nodes/AVLNode.kt
+++ lib/src/main/kotlin/bstrees/nodes/AVLNode.kt
@@ -4,5 +4,5 @@ class AVLNode<T : Comparable<T>>(override var data: T) : TreeNode<T, AVLNode<T>>
     override var parent: AVLNode<T>? = null
     override var left: AVLNode<T>? = null
     override var right: AVLNode<T>? = null
-    var height = 0
+    var height = 1
 }
