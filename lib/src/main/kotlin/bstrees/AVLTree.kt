ea2fd5a87879bcf33e808c974f63068dc216bf8a
Andrei <15161335+therain7@users.noreply.github.com>
2023-04-01 22:12:25 +0300

    feat(AVLTree): implement insert

--- lib/src/main/kotlin/bstrees/AVLTree.kt
+++ lib/src/main/kotlin/bstrees/AVLTree.kt
@@ -7,8 +7,10 @@ class AVLTree<T : Comparable<T>> : SelfBalancingBST<T, AVLNode<T>>() {
     override fun createNewNode(data: T) = AVLNode(data)
     override val balancer = AVLBalancer<T>()
 
+    /** Inserts new node with [data] as its value in the tree */
     override fun insert(data: T) {
-        TODO("Not yet implemented")
+        val insertedNode = insertNode(data)
+        treeRoot = balancer.balanceAfterInsertion(insertedNode)
     }
 
     override fun delete(data: T): T? {
