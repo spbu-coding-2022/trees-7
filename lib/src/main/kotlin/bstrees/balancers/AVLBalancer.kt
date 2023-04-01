ea204cac75efd08f6a40e7f589fbcc6239d599a7
Andrei <15161335+therain7@users.noreply.github.com>
2023-04-01 22:07:13 +0300

    feat(AVLBalancer): implement rotation methods

--- lib/src/main/kotlin/bstrees/balancers/AVLBalancer.kt
+++ lib/src/main/kotlin/bstrees/balancers/AVLBalancer.kt
@@ -1,8 +1,69 @@
 package bstrees.balancers
 
 import bstrees.nodes.AVLNode
+import kotlin.math.max
 
 class AVLBalancer<T : Comparable<T>> : TreeBalancer<T, AVLNode<T>> {
+    /**
+     * Rotates right edge of the [node] counterclockwise.
+     * Returns node that will be in place of the [node] passed
+     *
+     * Calls [updateHeight] to update heights of the nodes affected.
+     * Throws [IllegalArgumentException] if [node] without right child is passed
+     */
+    private fun rotateLeft(node: AVLNode<T>): AVLNode<T> {
+        val rightChild = node.right
+            ?: throw IllegalArgumentException("Node to rotate must have a right child")
+
+        rightChild.parent = node.parent
+        if (node.parent?.left == node) node.parent?.left = rightChild
+        else node.parent?.right = rightChild
+
+        node.right = rightChild.left
+        rightChild.left?.parent = node
+
+        rightChild.left = node
+        node.parent = rightChild
+
+        updateHeight(node)
+        updateHeight(rightChild)
+        return rightChild
+    }
+
+    /**
+     * Rotates left edge of the [node] clockwise.
+     * Returns node that will be in place of the [node] passed
+     *
+     * Calls [updateHeight] to update heights of the nodes affected.
+     * Throws [IllegalArgumentException] if [node] without left child is passed
+     */
+    private fun rotateRight(node: AVLNode<T>): AVLNode<T> {
+        val leftChild = node.left
+            ?: throw IllegalArgumentException("Node to rotate must have a left child")
+
+        leftChild.parent = node.parent
+        if (node.parent?.left == node) node.parent?.left = leftChild
+        else node.parent?.right = leftChild
+
+        node.left = leftChild.right
+        leftChild.right?.parent = node
+
+        leftChild.right = node
+        node.parent = leftChild
+
+        updateHeight(node)
+        updateHeight(leftChild)
+        return leftChild
+    }
+
+    /** Returns height of the [node] in AVL tree. Returns 0 if null passed */
+    private fun getHeight(node: AVLNode<T>?) = node?.height ?: 0
+
+    /** Updates height of the [node] in AVL tree */
+    private fun updateHeight(node: AVLNode<T>) {
+        node.height = max(getHeight(node.left), getHeight(node.right)) + 1
+    }
+
     override fun balanceAfterInsertion(node: AVLNode<T>): AVLNode<T> {
         TODO("Not yet implemented")
     }
