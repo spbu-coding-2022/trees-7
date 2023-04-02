fddbf958a49ef01116c2966ba9379589a2aeff56
aartdem <96226530+aartdem@users.noreply.github.com>
2023-04-02 16:21:18 +0300

    Add read-only public access to treeRoot (#10)
    * feat: add wrapped nodes
    
    * feat: public access to treeRoot

--- lib/src/main/kotlin/bstrees/AVLTree.kt
+++ lib/src/main/kotlin/bstrees/AVLTree.kt
@@ -2,8 +2,11 @@ package bstrees
 
 import bstrees.nodes.AVLNode
 import bstrees.balancers.AVLBalancer
+import bstrees.wrapped.WrappedAVLNode
 
-class AVLTree<T : Comparable<T>> : SelfBalancingBST<T, AVLNode<T>>() {
+class AVLTree<T : Comparable<T>> : SelfBalancingBST<T, AVLNode<T>, WrappedAVLNode<T>>() {
+    override val root get() = treeRoot?.let { WrappedAVLNode(it) }
+
     override fun createNewNode(data: T) = AVLNode(data)
     override val balancer = AVLBalancer<T>()
 
