91a5ed9b76968802029a407d98edb16005b6b890
aartdem <artdem243@gmail.com>
2023-04-02 15:10:02 +0300

    feat: add wrapped nodes

---
+++ lib/src/main/kotlin/bstrees/wrapped/WrappedRBNode.kt
@@ -0,0 +1,11 @@
+package bstrees.wrapped
+
+import bstrees.nodes.RBNode
+
+class WrappedRBNode<T : Comparable<T>>(private val nodeToWrap: RBNode<T>) : WrappedNode<T, WrappedRBNode<T>> {
+    override val data get() = nodeToWrap.data
+    override val parent get() = nodeToWrap.parent?.let { WrappedRBNode(it) }
+    override val left get() = nodeToWrap.left?.let { WrappedRBNode(it) }
+    override val right get() = nodeToWrap.right?.let { WrappedRBNode(it) }
+    val color get() = nodeToWrap.color
+}
