d4dbe226a67a95f3a8aa29afbd36c56ff5ff74b5
Ksenia Melashenko <112662076+ksenmel@users.noreply.github.com>
2023-04-03 19:14:49 +0300

    Implement delete operation in BSTree (#11)
     feat(SimpleBST): implement delete method

--- lib/src/main/kotlin/bstrees/SimpleBST.kt
+++ lib/src/main/kotlin/bstrees/SimpleBST.kt
@@ -8,7 +8,65 @@ class SimpleBST<T : Comparable<T>> : BinarySearchTree<T, SimpleNode<T>, WrappedS
 
     override fun createNewNode(data: T) = SimpleNode(data)
 
+    /** Deletes node and returns it as a result (or null). */
     override fun delete(data: T): T? {
-        TODO("Not yet implemented")
+        val nodeToDelete = searchNode(data) ?: return null
+
+        when {
+            nodeToDelete.left == null && nodeToDelete.right == null ->
+                deleteLeafNode(nodeToDelete)
+
+            nodeToDelete.left == null || nodeToDelete.right == null ->
+                deleteNodeWithOneChild(nodeToDelete)
+
+            else -> deleteNodeWithTwoChildren(nodeToDelete)
+        }
+
+        return nodeToDelete.data
+    }
+
+    /** The node to be deleted is a leaf node */
+    private fun deleteLeafNode(node: SimpleNode<T>) {
+        val parent = node.parent
+        if (parent != null) {
+            if (parent.left == node) parent.left = null
+            if (parent.right == node) parent.right = null
+        } else treeRoot = null
+    }
+
+    /** The node to be deleted has only one child. */
+    private fun deleteNodeWithOneChild(node: SimpleNode<T>) {
+        val parent = node.parent
+
+        if (parent == null) {
+            treeRoot = node.left ?: node.right
+            treeRoot?.parent = null
+        } else {
+            val nodeToReplaceWith = if (node.left == null) node.right!! else node.left!!
+            if (node == parent.right) parent.right = nodeToReplaceWith
+            else parent.left = nodeToReplaceWith
+
+            nodeToReplaceWith.parent = parent
+        }
+    }
+
+    /** Searches for node's successor until the node value is placed on the leaf of the tree. */
+    private fun findNodeToReplaceWith(node: SimpleNode<T>): SimpleNode<T> {
+        var nodeToReplaceWith = node.left!!
+        while (nodeToReplaceWith.right != null) {
+            nodeToReplaceWith = nodeToReplaceWith.right!!
+        }
+        return nodeToReplaceWith
+    }
+
+    /** The node to be deleted has two children. */
+    private fun deleteNodeWithTwoChildren(node: SimpleNode<T>) {
+        val nodeToReplaceWith = findNodeToReplaceWith(node)
+
+        node.data = nodeToReplaceWith.data
+
+        if (nodeToReplaceWith.left == null && nodeToReplaceWith.right == null)
+            deleteLeafNode(nodeToReplaceWith)
+        else deleteNodeWithOneChild(nodeToReplaceWith)
     }
 }
