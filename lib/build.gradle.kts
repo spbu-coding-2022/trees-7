6cc23866754a36d1d20cb8937e2ff4b5fdd33774
Andrei <15161335+therain7@users.noreply.github.com>
2023-04-06 02:34:26 +0300

    build: add JUnit 5 as dependency & configure gradle to use it

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -8,6 +8,10 @@ repositories {
 }
 
 dependencies {
-
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
+tasks.test {
+    useJUnitPlatform()
+}
