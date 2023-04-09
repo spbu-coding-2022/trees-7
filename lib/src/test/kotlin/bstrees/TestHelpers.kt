69739f20e8a591fa380d565109bcb338eed0d67d
aartdem <artdem243@gmail.com>
2023-04-09 15:15:26 +0300

    refactor: change helpers functions signature

--- lib/src/test/kotlin/bstrees/TestHelpers.kt
+++ lib/src/test/kotlin/bstrees/TestHelpers.kt
@@ -2,21 +2,27 @@ package bstrees
 
 import bstrees.nodes.TreeNode
 
-fun <T : Comparable<T>, NodeType : TreeNode<T, NodeType>> traverseInOrder(root: NodeType?, output: MutableList<T>) {
-    root?.let {
-        traverseInOrder(it.left, output)
-        output.add(it.data)
-        traverseInOrder(it.right, output)
+fun <T : Comparable<T>, NodeType : TreeNode<T, NodeType>> traverseInOrder(tree: BinarySearchTree<T, NodeType>): List<T> {
+    val treeList = mutableListOf<T>()
+
+    fun helper(node: NodeType?) {
+        node?.let {
+            helper(it.left)
+            treeList.add(it.data)
+            helper(it.right)
+        }
     }
+
+    helper(tree.root)
+
+    return treeList
 }
 
-fun <T : Comparable<T>, NodeType : TreeNode<T, NodeType>> checkBSTInvariant(root: NodeType?): Boolean {
-    root?.let {
-        val treeList = mutableListOf<T>().also { traverseInOrder(root, it) }
+fun <T : Comparable<T>, NodeType : TreeNode<T, NodeType>> checkBSTInvariant(tree: BinarySearchTree<T, NodeType>): Boolean {
+    val treeList = traverseInOrder(tree)
 
-        for (i in 1 until treeList.size)
-            if (treeList[i] <= treeList[i - 1]) return false
-    }
+    for (i in 1 until treeList.size)
+        if (treeList[i] <= treeList[i - 1]) return false
 
     return true
 }
