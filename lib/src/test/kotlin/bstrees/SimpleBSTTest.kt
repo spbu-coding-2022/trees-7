b6be3489c34d68d2cccc49df12dd87158ec62233
aartdem <artdem243@gmail.com>
2023-04-10 10:11:07 +0300

    refactor: add elementsCount - size of List for testing

--- lib/src/test/kotlin/bstrees/SimpleBSTTest.kt
+++ lib/src/test/kotlin/bstrees/SimpleBSTTest.kt
@@ -7,11 +7,14 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import kotlin.random.Random
 
-
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SimpleBSTTest {
     private val randomizer = Random(72)
-    private val values = Array(1000) { randomizer.nextInt() }.distinct()
+
+    // 3 is minimum possible count of elements
+    private val elementsCount = 5000
+
+    private val values = Array(elementsCount) { randomizer.nextInt() }.distinct()
     private lateinit var tree: SimpleBST<Int>
 
     @BeforeEach
@@ -37,7 +40,7 @@ class SimpleBSTTest {
     fun `invariant after insertion of duplicates`() {
         values.forEach(tree::insert)
 
-        values.slice(100..700).forEach {
+        values.slice(elementsCount / 3 until 2 * elementsCount / 3).forEach {
             tree.insert(it)
 
             assertTrue(checkBSTInvariant(tree), "BST invariant is not held")
@@ -62,7 +65,7 @@ class SimpleBSTTest {
     @Test
     fun `tree doesnt contain duplicate elements`() {
         values.forEach(tree::insert)
-        values.slice(250..720).forEach(tree::insert) // insert some elements again
+        values.slice(elementsCount / 3 until 2 * elementsCount / 3).forEach(tree::insert) // insert some elements again
 
         val treeElems = traverseInOrder(tree)
         assertTrue(
@@ -79,7 +82,7 @@ class SimpleBSTTest {
     fun `invariant after deletion`() {
         values.forEach(tree::insert)
 
-        values.slice(200..550).forEach {
+        values.slice(elementsCount / 4 until 3 * elementsCount / 4).forEach {
             tree.delete(it)
 
             assertTrue(checkBSTInvariant(tree), "BST invariant is not held")
@@ -89,7 +92,7 @@ class SimpleBSTTest {
     @Test
     fun `tree doesnt contain deleted elements`() {
         values.forEach(tree::insert)
-        val toDelete = values.slice(200..550)
+        val toDelete = values.slice(elementsCount / 4 until 3 * elementsCount / 4)
 
         toDelete.forEach(tree::delete) // delete some elements
 
@@ -128,9 +131,9 @@ class SimpleBSTTest {
 
     @Test
     fun `delete returns null when not found`() {
-        values.take(100).forEach(tree::insert)
+        values.take(elementsCount / 3).forEach(tree::insert)
 
-        values.takeLast(500).forEach {
+        values.takeLast(elementsCount / 3).forEach {
             assertEquals(
                 null, tree.delete(it),
                 "Delete method must return null if value to delete not found"
@@ -149,9 +152,9 @@ class SimpleBSTTest {
 
     @Test
     fun `search returns null when not found`() {
-        values.take(100).forEach(tree::insert)
+        values.take(elementsCount / 3).forEach(tree::insert)
 
-        values.takeLast(500).forEach {
+        values.takeLast(elementsCount / 3).forEach {
             assertEquals(
                 null, tree.search(it),
                 "Search method must return null if value not found"
