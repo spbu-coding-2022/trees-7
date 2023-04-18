c3cd5096aa206c5e10974892eea9e59f6d864447
Andrei <15161335+therain7@users.noreply.github.com>
2023-04-18 22:42:01 +0300

    feat: add Neo4jRepository to store trees in neo4j db

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ dependencyResolutionManagement {
             version("kotlin", "1.8.20")
             plugin("kotlin-jvm", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
             plugin("kotlin-serialization", "org.jetbrains.kotlin.plugin.serialization").versionRef("kotlin")
+            plugin("kotlin-noarg", "org.jetbrains.kotlin.plugin.noarg").versionRef("kotlin")
 
             library("kotlinx-serialization-json", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
@@ -14,6 +15,10 @@ dependencyResolutionManagement {
             library("exposed-core", "org.jetbrains.exposed", "exposed-core").versionRef("exposed")
             library("exposed-dao", "org.jetbrains.exposed", "exposed-dao").versionRef("exposed")
             library("exposed-jdbc", "org.jetbrains.exposed", "exposed-jdbc").versionRef("exposed")
+
+            version("neo4j-ogm", "4.0.5")
+            library("neo4j-ogm-core", "org.neo4j", "neo4j-ogm-core").versionRef("neo4j-ogm")
+            library("neo4j-ogm-bolt", "org.neo4j", "neo4j-ogm-bolt-driver").versionRef("neo4j-ogm")
 
             library("junit-jupiter", "org.junit.jupiter:junit-jupiter:5.9.2")
         }
