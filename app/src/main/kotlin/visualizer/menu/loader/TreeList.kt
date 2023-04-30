941c3f356799cdead697334c49343680d03a37de
Andrei <15161335+therain7@users.noreply.github.com>
2023-04-30 18:38:34 +0300

    feat(loader): handle text overflow

--- app/src/main/kotlin/visualizer/menu/loader/TreeList.kt
+++ app/src/main/kotlin/visualizer/menu/loader/TreeList.kt
@@ -1,14 +1,15 @@
 package visualizer.menu.loader
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import visualizer.commonui.AppButton
 import visualizer.commonui.defaultHeight
@@ -43,11 +44,14 @@ private fun TreeCard(modifier: Modifier = Modifier, tree: TreeCardData) {
         onClick = {}
     ) {
         Text(
+            modifier = Modifier.weight(1f),
             text = tree.name,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
             style = defaultTextStyle
         )
-        Spacer(Modifier.weight(1f))
         Text(
+            modifier = Modifier.padding(start = 20.dp),
             text = when (tree.type) {
                 TreeType.RB -> "Red-Black"
                 TreeType.AVL -> "AVL"
