05a8c7f79ef924d0abc30abbe62d0a5631f18e37
Andrei <15161335+therain7@users.noreply.github.com>
2023-04-30 17:57:52 +0300

    feat: add `NodeData` class to store key, value, x, y in trees
    Co-authored-by: aartdem <96226530+aartdem@users.noreply.github.com>

---
+++ app/src/main/kotlin/visualizer/NodeData.kt
@@ -0,0 +1,33 @@
+package visualizer
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+class NodeData(
+    val key: Int,
+    val value: String,
+    val x: Dp? = null,
+    val y: Dp? = null
+) : Comparable<NodeData> {
+    override fun compareTo(other: NodeData) = key.compareTo(other.key)
+
+    override fun toString(): String =
+        "key=$key value=$value\nx=$x y=$y"
+
+    companion object {
+        fun serialize(data: NodeData): String {
+            return "${data.x?.value};${data.y?.value};${data.key};${data.value}"
+        }
+
+        fun deserialize(data: String): NodeData =
+            data.split(";", limit = 4).let {
+                NodeData(
+                    key = it[2].toInt(),
+                    value = it[3],
+                    x = it[0].toFloatOrNull()?.dp,
+                    y = it[1].toFloatOrNull()?.dp
+                )
+            }
+    }
+}
+
